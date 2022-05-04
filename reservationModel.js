eb8364ce1710b4bfc6f824c23407c28a1261c067
chad-biersbach24 <90710906+chad-biersbach24@users.noreply.github.com>
2022-05-04 18:03:58 -0400

    Add files via upload

---
+++ reservationModel.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+
+const reservationSchema = mongoose.Schema({
+    confirmation_num:{
+        type: String,
+        required: [true, 'Please enter confirmation num']
+    },
+    name:{
+        type: String,
+        required: [true, 'Please enter user name']
+    },
+    email:{
+        type: String,
+        required: [true, 'Please enter user email']
+    },
+    flight_id:{
+        type: String,
+        required: [true, 'Please enter flight id']
+    },
+    from:{
+        type: String,
+        required: [true, 'Please enter flight origin']
+    },
+    to:{
+        type: String,
+        required: [true, 'Please enter flight destination']
+    },
+    departure_date:{
+        type: Date,
+        required: [true,'Please enter flight departure date']
+    },
+    arrival_date:{
+        type: Date,
+        required: [true,'Please enter flight arrival date']
+    },
+    departure_time:{
+        type:String,
+        required: [true, 'Please enter departure time']
+    },
+    arrival_time:{
+        type:String,
+        required: [true, 'Please enter arrival time']
+    },
+    rountrip:{
+        type: boolean,
+        required: [true, 'Please enter "false" for one-way or "true" for roundtrip']
+    },
+    flight_id2:{
+        type: String,
+        required: [true, 'Please enter flight id']
+    },
+    from2:{
+        type: String,
+        required: [true, 'Please enter flight origin']
+    },
+    to2:{
+        type: String,
+        required: [true, 'Please enter flight destination']
+    },
+    departure_date2:{
+        type: Date,
+        required: [true,'Please enter flight departure date']
+    },
+    arrival_date2:{
+        type: Date,
+        required: [true,'Please enter flight arrival date']
+    },
+    departure_time2:{
+        type:String,
+        required: [true, 'Please enter departure time']
+    },
+    arrival_time2:{
+        type:String,
+        required: [true, 'Please enter arrival time']
+    },
+    amount_paid:{
+        type: String,
+        required: [true, 'Please enter the amount paid']
+    }
+}, {
+    timestamps: true,
+})
+
+module.exports = mongoose.model('Reservation', reservationSchema)
